b5be90d12ae218e1967b3a289489f9669d6da106
dev-manish301 <rajzmanish94@gmail.com>
2025-08-16 12:00:18 +0530

    deleteting repo and intialiging new project

--- Day 23 (GPT Clone)/src/routes/index.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const authUser = require("../middleware/auth.middleware");
-const router = express.Router();
-
-router.get('/', authUser, (req, res) => {
-    res.render("home");
-});
-
-module.exports = router;
